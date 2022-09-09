8a8454ebb76c116245a4ae3847f11ebafcb2a03e
shobayo samuel <shobayosamuel62@gmail.com>
2022-09-10 12:31:38 +1300

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char ch;
+    int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+    for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
