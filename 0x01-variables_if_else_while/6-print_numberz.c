1505e37ca8d20dc48d641b046d7675cb680f493c
shobayo samuel <shobayosamuel62@gmail.com>
2022-09-10 13:38:12 +1300

    single digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints single digits from 0-10
- * followed by a new line
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num = 0;
+	int n;
 
-	while (num < 10)
-    {
-        putchar(num);
-        num++;
-    }
-    
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
 	putchar('\n');
 	return (0);
 }
