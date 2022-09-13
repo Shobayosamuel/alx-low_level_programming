3e409e88b1334c3219b9c8fc35a7deed0f8f0a0e
shobayo samuel <shobayosamuel62@gmail.com>
2022-09-14 11:06:15 +1300

    functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,5 +15,6 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - Entry point
+ * Return:0
  */
-void print_alphabet(void);
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+
 void print_alphabet(void)
 {
-    char c = 'a';
+	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
+
 	}
 	_putchar('\n');
+
 }
