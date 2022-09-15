c2be8faba8255d19ddc7c876d28653e8d7285737
shobayo samuel <shobayosamuel62@gmail.com>
2022-09-16 09:02:06 +1300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
+ *  _isupper - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int c)
+int  _isupper(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return (c >= 'A' && c <= 'Z');
 }
