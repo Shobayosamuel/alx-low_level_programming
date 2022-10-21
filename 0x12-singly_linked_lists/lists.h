7f0ef3403baf0189507bc890d85a7544ae17ab82
shobayo samuel <shobayosamuel62@gmail.com>
2022-10-21 11:53:24 +0100

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -7,12 +8,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
