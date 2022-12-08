37bd3c58aaef259f0be961dd8d30e36906b891aa
shobayo samuel <shobayosamuel62@gmail.com>
2022-12-08 21:21:15 +0100

    free linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,17 @@
 #include "lists.h"
+
 /**
  * free_dlistint - Frees a linked dlistint_t list.
  * @head: The head of the dlistint_t list.
  */
-
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *curr = head;
-    dlistint_t *temp;
-    while (curr != NULL)
-    {
-        temp = curr;
-        curr = curr->next;
-        free(curr);
-    }
-    head = NULL;
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
