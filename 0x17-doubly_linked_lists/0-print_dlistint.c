7ae22cf01cbc6e053438b79dfa0d0d511bce2d53
shobayo samuel <shobayosamuel62@gmail.com>
2022-12-08 19:28:16 +0100

    corrected print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num = 0;
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		num++;
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (num);
